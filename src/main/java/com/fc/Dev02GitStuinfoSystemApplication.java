586cab1cbca090ab67abebbdda3972665547778f
gpy <3513972901@qq.com>
2022-05-04 18:26:28 +0800

    初始化仓库

---
+++ src/main/java/com/fc/Dev02GitStuinfoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev02GitStuinfoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dev02GitStuinfoSystemApplication.class, args);
+    }
+
+}
